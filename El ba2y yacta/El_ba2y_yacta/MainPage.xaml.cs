8260fd328d410d77190355f0247a476cac8ed4d8
medo gad <medogad5033@gmail.com>
2020-06-19 22:06:56 +0200

    last version

---
+++ El ba2y yacta/El_ba2y_yacta/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace El_ba2y_yacta
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
